18e15c55f8464a28fcc4a92bc81734a18a0c19e4
Tom Servo <servo@servo-mac3.local>
2016-08-18 21:18:34 -0400

    Version bump: 2016.08.19

---
+++ Formula/servo-bin.rb
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class ServoBin < Formula
+  desc "Servo, the Parallel Browser Engine Project (binary version)"
+  homepage "http://servo.org"
+  url "https://download.servo.org/nightly/macbrew/servo-2016-08-19.tar.gz"
+  version "2016.08.19"
+  sha256 "d902d50bb7a33577e0441787d08954629e41b6f07d12f7c60035f3a188c30aa3"
+
+  bottle :unneeded
+
+  def install
+    prefix.install "bin", "libexec", "resources"
+  end
+
+  test do
+    system bin/"servo", "-o", "/dev/null", "-x", "http://example.com"
+  end
+end
