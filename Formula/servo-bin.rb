8aac7bd71ddeca6a8dd6c296852327cc30ef662a
Tom Servo <servo@servo.org>
2017-04-23 21:08:42 -0400

    Version bump: 2017.04.24

--- Formula/servo-bin.rb
+++ Formula/servo-bin.rb
@@ -5,9 +5,9 @@
 class ServoBin < Formula
   desc "Servo, the Parallel Browser Engine Project (binary version)"
   homepage "http://servo.org"
-  url "https://download.servo.org/nightly/macbrew/2017-04-23T01-08-58Z-servo.tar.gz"
-  version "2017.04.23"
-  sha256 "25c1b19da113efc4030a7ab1687c35ae911b9b50d9f869083852447949ebf6d1"
+  url "https://download.servo.org/nightly/macbrew/2017-04-24T01-08-34Z-servo.tar.gz"
+  version "2017.04.24"
+  sha256 "7be0c8accf760effcf7a4240dbdda8b32fdb066dc8457b33cfe36b2344d33936"
 
   bottle :unneeded
 
